dc6660c08324e62076bd4e8c7001c36a76943bce
hientran-sudo <hien.tran@student.fairfield.edu>
2021-08-26 14:24:16 -0400

    change in student page

---
+++ SMS/Controllers/EnrollmentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SMS.Controllers
+{
+    public class EnrollmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
